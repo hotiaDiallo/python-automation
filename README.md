f1b485d204b5669ed298c7e82e23ebe0b7da1315
hotiaDiallo <ibrahimahotia.diallo@gmail.com>
2022-10-31 16:06:01 +0100

    update : EKS clusters status checker

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Python automation for devops engineer 
 
-## [EC2 instances checker](https://github.com/hotiaDiallo/python-automation/blob/main/ec2-status-checker.py)
+## [EC2 instances status checker](https://github.com/hotiaDiallo/python-automation/blob/main/ec2-status-checker.py)
 
 ## [Add Environment tags on EC2 instances](https://github.com/hotiaDiallo/python-automation/blob/main/add-enviroment-tags.py)
+
+## [EKS clusters status checker](https://github.com/hotiaDiallo/python-automation/blob/main/eks-status-checker.py)
 
 ## Next : coming soon
--- eks-status-checker.py
+++ eks-status-checker.py
@@ -1,17 +1,27 @@
 import boto3
+import schedule
 
 eks_client = boto3.client('eks', region_name="eu-west-3")
 eks_clusters = eks_client.list_clusters().get('clusters')
 
-for cluster in eks_clusters:
-    response = eks_client.describe_cluster(
-        name=cluster
-    )
-    cluster_info = response.get('cluster')
-    cluster_status = cluster_info.get('status')
-    cluster_endpoint = cluster_info.get('endpoint')
-    cluster_version = cluster_info.get('version')
 
-    print(f"Cluster {cluster} status is {cluster_status}")
-    print(f"Cluster endpoint: {cluster_endpoint}")
-    print(f"Cluster version: {cluster_version}")
+def check_eks_clusters_status():
+    for cluster in eks_clusters:
+        response = eks_client.describe_cluster(
+            name=cluster
+        )
+        cluster_info = response.get('cluster')
+        cluster_status = cluster_info.get('status')
+        cluster_endpoint = cluster_info.get('endpoint')
+        cluster_version = cluster_info.get('version')
+
+        print(f"Cluster {cluster} status is {cluster_status}")
+        print(f"Cluster endpoint: {cluster_endpoint}")
+        print(f"Cluster version: {cluster_version}")
+
+
+schedule.every(5).hours.do(check_eks_clusters_status)
+
+while True:
+    schedule.run_pending()
+
